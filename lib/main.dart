c879b42f32eca889010a75c2fe957a8400615c32
tlaw22 <timothy.j.lawless@outlook.com>
2023-06-19 09:36:01 -0400

    App is working 100%. Next I need to learn how to do this with a data class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Load data from JSON into GUI',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Load data from JSON into GUI'),
     );
   }
 }
@@ -31,11 +31,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List _items = [];
   int _counter = 0;
 
-  void _incrementCounter() {
+  // Fetch content from the json file
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString("assets/sample.json");
+    final data = await jsonDecode(response);
     setState(() {
-      _counter++;
+      _items = data["items"];
+      print(".. number of item: ${_items.length}");
     });
   }
 
@@ -45,27 +50,36 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(itemCount: _items.length, itemBuilder: (BuildContext context, int index){
+              return Card(
+                key: ValueKey(_items[index]["id"]),
+                margin: EdgeInsets.all(10),
+                color: Colors.indigo,
+                child: ListTile(textColor: Colors.white,
+                    leading: Text(_items[index]["id"]),
+                    title: Text(_items[index]["name"]),
+                    subtitle: Text(_items[index]["description"])
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
+      bottomSheet:
+      Container(
+        height: 100,
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {
+            readJson();
+          },
+          child: const Text('Load Json'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
